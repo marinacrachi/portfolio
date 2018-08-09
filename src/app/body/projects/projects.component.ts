7ba957eaea86e384e39fdbdb011f04c7ad58be05
Marina Crachi <marinacrachi@gmail.com>
2018-08-09 17:55:47 -0300

    about and projects

--- src/app/body/projects/projects.component.ts
+++ src/app/body/projects/projects.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  mostra1 = false;
+  mostra2 = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  mostra(div){
+    switch(div){
+      
+      case 1:
+      {
+        this.mostra1 = true;
+        this.mostra2 = false;
+      }
+      break;
+      
+      case 2:
+      {
+        this.mostra1 = false;
+        this.mostra2 = true;
+      }
+      break;
+
+      default:
+      {
+        this.mostra1 = false;
+        this.mostra2 = false;
+      }
+      break;
+    } 
   }
 
 }
